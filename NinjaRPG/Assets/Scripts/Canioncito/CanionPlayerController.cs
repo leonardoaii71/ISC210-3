18e9afe87e9b45ab1c9c7d8cb3e5dfc5db9602d2
Leonardo Castro <leonardoaii71@gmail.com>
2020-03-10 20:27:07 -0400

    canion updated

--- NinjaRPG/Assets/Scripts/Canioncito/CanionPlayerController.cs
+++ NinjaRPG/Assets/Scripts/Canioncito/CanionPlayerController.cs
@@ -6,17 +6,23 @@ public class CanionPlayerController : MonoBehaviour
 {
     
     const float LEFTLIMIT = -7.1f, RIGHTLIMIT = 7.1f;
-    float speed = 10f;
-    float triggerSpeed = 10f, triggerAngle;
+    float speed = 10f, barValue;
+    float triggerSpeed = 30f, triggerAngle;
     Vector3 deltaPos, mousePosition;
     public GameObject CanionBallPrefab;
+    public  GameObject ProgressBar;
+    LineRenderer trajectory;
+    const int trajectoryVertices = 20;
 
-
+    private void Awake() {
+        trajectory = GetComponent<LineRenderer>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         deltaPos = new Vector3();
+        trajectory.positionCount = trajectoryVertices;
     }
 
     // Update is called once per frame
@@ -41,15 +47,36 @@ public class CanionPlayerController : MonoBehaviour
         else if (deltaPos.y < 0){
             triggerAngle = 0;
         }
-
-        Debug.Log(Mathf.PingPong(0, 20));
-        //
         
 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Instantiate(CanionBallPrefab, transform.position, Quaternion.identity).GetComponent<CanionBallBehaviour>().Shoot(triggerSpeed, triggerAngle);
-        }
+        if (Input.GetButton("Fire1"))
+        { 
+            barValue = Mathf.PingPong(Time.time, 1) * 100f;
+            ProgressBar.GetComponent<ProgressBar>().BarValue = barValue;
             
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        { 
+            Instantiate(CanionBallPrefab, transform.position, Quaternion.identity)
+                .GetComponent<CanionBallBehaviour>()
+                .Shoot(triggerSpeed * (barValue / 100), triggerAngle);
+        }
+/*
+        for (int i = 0; i < trajectoryVertices; i++)
+        {
+            trajectory.SetPosition(i, GetPosition((float)i 
+            / trajectoryVertices, Mathf.Pow(triggerSpeed, 2) * Mathf.Sin(triggerAngle * 2) 
+            / (Physics.gravity.y)));
+        }
+*/
     }
+    Vector3 GetPosition(float resolutionProportion, float xMax)
+        {
+            float xRelative = resolutionProportion * xMax;
+            float yRelative = xRelative * Mathf.Tan(triggerAngle) - (Mathf.Abs(Physics.gravity.y) * Mathf.Pow(xRelative, 2)) 
+            / (2 * (triggerSpeed * (barValue / 100) )) *  (triggerSpeed * (barValue / 100)) * Mathf.Cos(Mathf.Pow(triggerAngle, 2));
+
+            return new Vector3(xRelative, yRelative);
+        }
 }
